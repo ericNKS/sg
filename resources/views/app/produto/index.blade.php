17c997229e950d8628d9cf0a99ca264748c2329f
Eric Santos <ek.silva.santos@gmail.com>
2023-04-07 16:33:01 -0300

    att

--- resources/views/app/produto/index.blade.php
+++ resources/views/app/produto/index.blade.php
@@ -9,7 +9,7 @@
     <div class="menu">
         <ul>
             <li>
-                <a href="">Novo</a>
+                <a href="{{route('produto.create')}}">Novo</a>
             </li>
             <li>
                 <a href="">Consulta</a>
